52a5857b65295aa4c28b237902869a14b2582f6b
Wleydson Lemos <wleydson.lemos@boavista.local>
2020-08-27 19:16:04 -0300

    initial commit

---
+++ src/test/java/com/wleydsonlemos/workshopmongo/WorkshopMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wleydsonlemos.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
