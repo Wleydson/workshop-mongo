ba5a3152d639ed30063aecf5b1f880b39f9af9c6
Wleydson Lemos <wleydsonredes@gmail.com>
2020-08-30 10:28:56 -0300

    URL decode and find by title

--- src/main/java/com/wleydsonlemos/workshopmongo/resources/PostResource.java
+++ src/main/java/com/wleydsonlemos/workshopmongo/resources/PostResource.java
@@ -1,15 +1,20 @@
 package com.wleydsonlemos.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wleydsonlemos.workshopmongo.domain.Post;
 import com.wleydsonlemos.workshopmongo.service.PostService;
 
 import lombok.RequiredArgsConstructor;
+
+import static com.wleydsonlemos.workshopmongo.resources.util.URL.decodeParam;
 
 @RequiredArgsConstructor
 @RestController
@@ -23,6 +28,14 @@ public class PostResource {
 		Post post = service.findById(id);
 		
 		return ResponseEntity.ok().body( post );
+	}
+	
+	@GetMapping(value = "/title")
+	public ResponseEntity< List<Post> > findByTitle(@RequestParam(value = "text", defaultValue = "") String title){
+		String titleDecode = decodeParam(title);
+		List<Post> posts = service.findByTitle(titleDecode);
+		
+		return ResponseEntity.ok().body( posts );
 	}
 	
 }
--- src/main/java/com/wleydsonlemos/workshopmongo/service/PostService.java
+++ src/main/java/com/wleydsonlemos/workshopmongo/service/PostService.java
@@ -1,5 +1,6 @@
 package com.wleydsonlemos.workshopmongo.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,10 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> post = repository.findById(id);
 		return post.orElseThrow(() -> new ObjectNotFoundException("Post not found"));
+	}
+	
+	public List<Post> findByTitle(String title) {
+		return repository.findByTitleContainingIgnoreCase(title);
 	}
 	
 	
