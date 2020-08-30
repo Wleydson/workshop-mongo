028a36bfccb8907e9805c1b871d947a15ba657bd
Wleydson Lemos <wleydsonredes@gmail.com>
2020-08-30 09:06:15 -0300

    referencing post with user

--- src/main/java/com/wleydsonlemos/workshopmongo/domain/User.java
+++ src/main/java/com/wleydsonlemos/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.wleydsonlemos.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
@@ -24,4 +27,9 @@ public class User implements Serializable{
 	private String name;
 	
 	private String email;
+	
+	@DBRef(lazy = true)
+	@Builder.Default
+	private List<Post> posts = new ArrayList<>();
+
 }
